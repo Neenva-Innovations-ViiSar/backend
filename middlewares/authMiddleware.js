0ad216224ca452f1e29150a060f7571ec4338445
Apurva Srivastava <apurvasrivastava2403@gmail.com>
2025-04-14 14:22:44 +0530

    password correction

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -17,7 +17,7 @@ const userValidation = (req, res, next) => {
 const passwordValidation = (req, res, next) => {
   const schema = Joi.object({
     userId: Joi.string().required(), // Allowing userId now
-    password: Joi.string().min(6).required()
+    password: Joi.string().min(4).required()
   });
 
   const { error } = schema.validate(req.body);
