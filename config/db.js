acef5db168ef0ad3966fabe7447de00c92d80b1d
Apurva Srivastava <apurvasrivastava2403@gmail.com>
2025-05-29 21:22:45 +0530

    updated .env

--- config/db.js
+++ config/db.js
@@ -1,10 +1,17 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-const mongo_url = process.env.MONGO_CONN;
+const username = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
+const cluster = process.env.MONGO_CLUSTER;
+const dbName = process.env.MONGO_DB;
+
+const mongo_url = `mongodb+srv://${username}:${password}@${cluster}/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose.connect(mongo_url)
-    .then(()=>{
+    .then(() => {
         console.log("MongoDB connected...");
-    }).catch((err)=>{
-        console.log("MongoDb Connection error: ",err);
+    })
+    .catch((err) => {
+        console.log("MongoDB Connection error:", err);
     });
