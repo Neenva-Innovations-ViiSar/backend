79fb8aec979de63658703a959bd078da228fc64f
Apurva Srivastava <apurvasrivastava2403@gmail.com>
2025-06-04 22:49:31 +0530

    topic model changes

--- models/Topic.js
+++ models/Topic.js
@@ -5,13 +5,26 @@ const topicSchema = new mongoose.Schema(
     name: { type: String, required: true },
     lang: { type: String, required: true },
     audioUrl: { type: String, required: true },
+    
+    // New field to differentiate between topic and reel
+    type: {
+      type: String,
+      enum: ['topic', 'reel'],
+      default: 'topic',
+    },
+
+    voiceGiver: { type: String },
+
+    // These are required only for 'topic' - chapter related
     chapterId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Chapter",
-      required: true,
     },
-    levelId: { type: mongoose.Schema.Types.ObjectId, ref: 'Level', required: true },
-    sequence: { type: Number, required: true } // for sequencing the audio.
+    levelId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Level",
+    },
+    sequence: { type: Number }, // For ordering topic audios
   },
   { timestamps: true }
 );
