d2ae1881bbb692e9e9f1bfb5ef413f8e08c1cebf
Apurva Srivastava <apurvasrivastava2403@gmail.com>
2025-06-12 20:37:48 +0530

    getlevelcontent

--- routes/levelRoutes.js
+++ routes/levelRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {
   createLevel,
   getLevelsByChapter,
+  getLevelContent,
 } = require("../controllers/levelController");
 const { verifyToken } = require("../middlewares/authMiddleware");
 
@@ -12,6 +13,6 @@ router.post("/new", createLevel);
 // Get all levels under a specific chapter
 router.get("/:chapterId", verifyToken, getLevelsByChapter);
 
-router.get("/:levelId/contents", verifyToken);
+router.get("/:levelId/contents", verifyToken, getLevelContent);
 
 module.exports = router;
